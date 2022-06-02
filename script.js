c901a58b07abf77d9c6b1a522234ee1b42287399
CoreyAI <coreyiaquinta@gmail.com>
2022-06-02 18:37:00 -0400

    Added CSS. Modified HTML and JS to display grid with hover properties to alter the color of the squares.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-let grid_width = 16;
-let grid_height = 16;
+const container = document.querySelector('.container');
 
-let grid = document.createElement("div");
-grid.textContent = "test";
+function createGrid() {
+    let grid_columns = 16;
+    let grid_rows = 16;
+    let grid_area = grid_columns * grid_rows;
+
+    for (i = 1; i < grid_area + 1; i++) {
+        const square = document.createElement('div');
+        square.className = "square";
+        square.id = `s${i}`;
+        container.appendChild(square);
+    }
+}
+
+function hoverGrid() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener('mouseout', changeColor);
+    });
+}
+
+function changeColor(e) {
+    const square = document.getElementById(this.id);
+    square.style.backgroundColor = "black";
+}
+
+createGrid();
+hoverGrid();
